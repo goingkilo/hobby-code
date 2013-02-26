e6a1772d877f429eaef15866cf4bbbbd0be22800
karthik raghunathan <raghunathan.karthik@gmail.com>
2013-02-25 23:42:06 -0600

    added an eval function .
    button now evals all code in the text pane.
    output goes to stdout

--- edi.clj
+++ edi.clj
@@ -20,6 +20,46 @@
 
 (def act (proxy [ActionListener] []
            (actionPerformed [event]
+                            (println "button->" (eval (read-string (.getText text)))))))
+
+(def key-listen
+    (proxy [KeyListener] []
+        (keyPressed [event])
+         (keyReleased [event] )
+         (keyTyped [event] )))
+
+
+(. text setCaretColor Color/RED)
+(. text addCaretListener caret-listener)
+(.. text getCaret  (setBlinkRate 600))
+(. text setText  "start hacking")
+
+(.add panel button)
+(.add panel (new JScrollPane text))
+
+(.add frame panel)
+
+(doto frame
+   (.setSize  350 500)
+ (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+ (.setLocationRelativeTo nil )
+   (.setVisible true))
+
+ 
+
+(.addActionListener button act)
+(.addKeyListener text key-listen)
+
+
+
+(.setLayout panel (BoxLayout. panel BoxLayout/Y_AXIS))
+
+(def caret-listener (proxy [CaretListener] []
+ (caretUpdate [caretEvent]
+  (println "you have a caret update ! good for eyes ! " (.getDot caretEvent))) )   )
+
+(def act (proxy [ActionListener] []
+           (actionPerformed [event]
                             (let [a (or (.getSelectedText text) ";;")]
                                         (println  " you clicked button")))))
 
