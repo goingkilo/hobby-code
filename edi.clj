9960673935ba250401e89a64793f856e496ffd4b
karthik raghunathan <raghunathan.karthik@gmail.com>
2013-02-25 21:02:04 -0600

    added caret listener

--- edi.clj
+++ edi.clj
@@ -1,44 +1,50 @@
-
-(ns pipo)
-
 (import '(javax.swing JFrame JPanel JButton JOptionPane JTextPane JScrollPane BoxLayout))
-(import '(java.awt BorderLayout))
+(import '(java.awt BorderLayout Color Dimension))
 (import '(java.awt.event ActionListener  KeyEvent KeyListener))
 (import '(java.io PushbackReader BufferedReader))
+(import javax.swing.event.CaretListener)
+(import '(javax.swing.text BadLocationException DefaultHighlighter Highlighter))
 
-(def frame (doto (JFrame. "Hello Frame")))
+(def frame  (JFrame. "Clojure"))
 (def panel (JPanel. ))
 (def text (JTextPane.))
 (def scroll (JScrollPane. text))
-(def button (JButton. "Click Me!"))
+(def button (JButton. "evaluate"))
 
 
 (.setLayout panel (BoxLayout. panel BoxLayout/Y_AXIS))
 
-(.add panel scroll BorderLayout/CENTER)
-(.add panel button BorderLayout/SOUTH)
+(def caret-listener (proxy [CaretListener] []
+ (caretUpdate [caretEvent]
+  (println "you have a caret update ! good for eyes ! " (.getDot caretEvent))) )   )
 
-(.add (.getContentPane frame) panel) 
+(def act (proxy [ActionListener] []
+           (actionPerformed [event]
+                            (let [a (or (.getSelectedText text) ";;")]
+                                        (println  " you clicked button")))))
 
-;;(.. frame (getContentPane) (add panel))
+(def key-listen
+    (proxy [KeyListener] []
+        (keyPressed [event])
+         (keyReleased [event] )
+         (keyTyped [event] )))
+
+
+(. text setCaretColor Color/RED)
+(. text addCaretListener caret-listener)
+(.. text getCaret  (setBlinkRate 600))
+(. text setText  "start hacking")
+
+(.add panel (new JScrollPane text))
+
+(.add frame panel)
 
 
 (doto frame
-  (.setSize 500 600)
-	(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-	(.setLocationRelativeTo nil )
-  (.setVisible true))
-
-(def act (proxy [ActionListener] []
-           (actionPerformed [event] 
-                            (let [a (or (.getSelectedText text) ";;")]
-                                        (println (eval (read-string a)))))))
-
-(def key-listen 
-    (proxy [KeyListener] []
-	       (keyPressed [event])
-         (keyReleased [event] )
-         (keyTyped [event] )))
+   (.setSize  350 500)
+ (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+ (.setLocationRelativeTo nil )
+   (.setVisible true))
 
 
 (.addActionListener button act)
