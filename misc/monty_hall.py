e637ddfd7e2090ea610b4d2e6fcf79d5a67e8338
Karthik Raghunathan <kraghunathan@Groupons-MacBook-Pro-2.local>
2015-10-19 15:26:26 +0530

    ...

--- misc/monty_hall.py
+++ misc/monty_hall.py
@@ -14,13 +14,14 @@ Such zen
 import random
 
 pick = lambda x : random.randint(0,x)
-
+# car is 1 in 3, user picks 1 in 3, monty eliminates 1 in 3, user switches or not, 
 def go(d):
         car = pick(2)
         choice1 = pick(2)
 
         a = [0,1,2]
-        a.remove(choice1)
+	print choice1
+        a.remove( choice1)
         monty1 = [x for x in a if not car == x][0]
         a.remove(monty1)
         choice2 = a[0]
@@ -28,6 +29,7 @@ def go(d):
                 print "car, choice1, monty-door, choice2 "
                 print car, choice1, monty1, choice2
         return car, choice1, monty1, choice2
+
 switchwins = 0
 noswitchwins = 0
 for i in range(1000):
@@ -36,4 +38,5 @@ for i in range(1000):
                 switchwins += 1
         if car == choice1:
                 noswitchwins += 1
+print 'switchwins,no switchwins' 
 print switchwins ,'/', noswitchwins
